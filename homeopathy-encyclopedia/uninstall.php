6663924b8021cdf5dc2ac9907284fb18ef72e763
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-10 20:16:58 +0500

    chore(file-06): include v2.3 future intelligence in guarded uninstall

--- homeopathy-encyclopedia/uninstall.php
+++ homeopathy-encyclopedia/uninstall.php
@@ -21,7 +21,11 @@ if ( is_multisite() ) {
 function he_v2_purge_site() {
 	global $wpdb;
 	wp_clear_scheduled_hook( 'he_v2_maintenance' );
-	foreach ( array( 'concepts','aliases','versions','references','relations','reviews','integrity_actions','research','dataset_access','events','outbox','idempotency','search_index','bookmarks','feedback','audit_log','metrics','rate_limits','migration_quarantine' ) as $suffix ) {
+	wp_clear_scheduled_hook( 'he_v23_future_refresh' );
+	foreach ( array(
+		'concepts','aliases','versions','references','relations','reviews','integrity_actions','research','dataset_access','events','outbox','idempotency','search_index','bookmarks','feedback','audit_log','metrics','rate_limits','migration_quarantine',
+		'future_claims','future_claim_evidence','future_provenance','future_external_sources','future_watchers','future_translations','future_freshness','future_impact_queue','future_research_gaps'
+	) as $suffix ) {
 		$table = $wpdb->prefix . 'he_' . $suffix;
 		$wpdb->query( "DROP TABLE IF EXISTS `{$table}`" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 	}
@@ -36,7 +40,7 @@ function he_v2_purge_site() {
 	}
 	foreach ( array(
 		'he_schema_version','he_v2_runtime_failure','he_v2_safe_mode','he_v2_migration_lock','he_v2_legacy_migrated','he_allow_destructive_uninstall','he_page_map','he_legacy_system_migration',
-		'he_v22_extension_version','he_v22_upgrade_lock','he_v22_legacy_cursor','he_v22_legacy_done','he_v22_reindex_cursor','he_v22_reindex_required'
+		'he_v22_extension_version','he_v22_upgrade_lock','he_v22_legacy_cursor','he_v22_legacy_done','he_v22_reindex_cursor','he_v22_reindex_required','he_v23_future_schema'
 	) as $option ) {
 		delete_option( $option );
 	}
